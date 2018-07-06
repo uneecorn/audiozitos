3ada67893d386b078fd2119555f2dacbad1d2981
Tyler Mendes de Brito <tylerbryto@gmail.com>
2018-07-05 22:55:14 -0300

    adding recording button prototype

--- lib/ui/recorder_page.dart
+++ lib/ui/recorder_page.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 
-class RecorderPage extends StatelessWidget {
+class RecorderPage extends StatefulWidget {
+  @override
+  _RecorderPageState createState() => _RecorderPageState();
+}
+
+class _RecorderPageState extends State<RecorderPage> {
+  bool _isRecording = false;
+
+  IconData get _startStopButtonIcon =>
+      _isRecording ? Icons.stop : Icons.play_arrow;
+
+  void _startRecording() {
+    setState(() {
+      _isRecording = true;
+    });
+  }
+
+  void _stopRecording() {
+    setState(() {
+      _isRecording = false;
+    });
+  }
+
+  void _onStartStopButtonPressed() {
+    if (_isRecording)
+      _stopRecording();
+    else
+      _startRecording();
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         //App Bar
@@ -15,7 +44,28 @@ class RecorderPage extends StatelessWidget {
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Recorder page!'),
+                Center(
+                  child: Material(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(75.0),
+                    child: Container(
+                      height: 150.0,
+                      width: 150.0,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(75.0),
+                        splashColor: Theme.of(context).accentColor,
+                        highlightColor: Theme.of(context).accentColor,
+                        onTap: _onStartStopButtonPressed,
+                        child: Center(
+                          child: Icon(
+                            _startStopButtonIcon,
+                            size: 60.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             )
           ],
